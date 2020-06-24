788190cbc42e05581f2eb56b5f50f9a5cf64809d
Aaron Julian <aaron.julian@sapns2.com>
2020-06-23 20:54:24 -0400

    Adding functionality across the board

--- public/js/UI_Components/sideBar.js
+++ public/js/UI_Components/sideBar.js
@@ -1,7 +1,7 @@
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
 function openNav() {
-    document.getElementById("mySidenav").style.width = "350px";
-    document.getElementById("main").style.marginLeft = "350px";
+    document.getElementById("mySidenav").style.width = "380px";
+    document.getElementById("main").style.marginLeft = "50px";
   }
   
   /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
