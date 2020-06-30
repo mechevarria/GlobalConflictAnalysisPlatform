ee893b84211fccf6f07899cca6eacabe5099b4be
Mike Echevarria <mike.echevarria@gmail.com>
2020-06-30 13:06:40 -0400

    fix typo in env set

--- cf-push-buildpack.sh
+++ cf-push-buildpack.sh
@@ -8,9 +8,9 @@ cf push $app \
     -m 128M \
     -k 1048M
 
-cf se $app HDB_HOST=$HDB_HOST
-cf se $app HDB_PORT=$HDB_PORT
-cf se $app HDB_USER=$HDB_USER
+cf se $app HDB_HOST $HDB_HOST
+cf se $app HDB_PORT $HDB_PORT
+cf se $app HDB_USER $HDB_USER
 cf se $app HDB_PASSWORD $HDB_PASSWORD
 cf se $app MAPBOX_TOKEN $MAPBOX_TOKEN
 
