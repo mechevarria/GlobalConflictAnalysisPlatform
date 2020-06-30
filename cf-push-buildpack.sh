12c7463a568a33ce16ce84d7b2f64cd884386818
Mike Echevarria <mike.echevarria@gmail.com>
2020-06-30 12:57:48 -0400

    add variables for deployment

--- cf-push-buildpack.sh
+++ cf-push-buildpack.sh
@@ -1,35 +1,17 @@
 #!/usr/bin/env bash
 
-status=$(cf app keycloak-coreui | sed -n 3p)
-if [[ $status = "FAILED" ]]; then
-  exit 1
-else
-  keycloak_url=https://$(cf app keycloak-coreui | awk '{print $2}' | sed -n 5p)/auth
-  echo keycloak_url=$keycloak_url
-fi
-
-service=hdi_hana
-status=$(cf service $service | sed -n 3p)
-if [[ $status = "FAILED" ]]; then
-  exit 1
-fi
-
-app=express-api
-
-cd $app
+app=conflict-analysis
 
 cf push $app \
     -b nodejs_buildpack \
     --no-start \
-    -m 64M \
-    -k 2048M
+    -m 128M \
+    -k 1048M
 
-cf se $app KEYCLOAK_URL $keycloak_url
-cf se $app KEYCLOAK false
-
-# bind hana service
-cf bind-service $app $service
+cf se $app HDB_HOST=$HDB_HOST
+cf se $app HDB_PORT=$HDB_PORT
+cf se $app HDB_USER=$HDB_USER
+cf se $app HDB_PASSWORD $HDB_PASSWORD
+cf se $app MAPBOX_TOKEN $MAPBOX_TOKEN
 
 cf start $app
-
-cd ..
