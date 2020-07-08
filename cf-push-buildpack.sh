6a24b7739c2b8ca66639d7af6145a89e69ddbddb
Mike Echevarria <mike.echevarria@gmail.com>
2020-07-08 12:05:27 -0400

    no longer using mapbox

--- cf-push-buildpack.sh
+++ cf-push-buildpack.sh
@@ -12,6 +12,5 @@ cf se $app HDB_HOST $HDB_HOST
 cf se $app HDB_PORT $HDB_PORT
 cf se $app HDB_USER $HDB_USER
 cf se $app HDB_PASSWORD $HDB_PASSWORD
-cf se $app MAPBOX_TOKEN $MAPBOX_TOKEN
 
 cf start $app
