0af301eb928b464ae992b1a25594d7a9ae15f18a
Mike Echevarria <mike.echevarria@gmail.com>
2020-08-14 11:04:41 -0400

    control values of checkboxes

--- public/js/button-handlers.js
+++ public/js/button-handlers.js
@@ -9,6 +9,13 @@ class ButtonHandlers {
         this.notesBtn = document.getElementById('notes');
         this.cancelBtn = document.getElementById('cancel');
         this.updateBtn = document.getElementById('update');
+        this.covidSwitch = document.getElementById('covid-switch');
+        this.battlesCheck = document.getElementById('battles-check');
+        this.explosionsCheck = document.getElementById('explosions-check');
+        this.protestsCheck = document.getElementById('protests-check');
+        this.riotsCheck = document.getElementById('riots-check');
+        this.strategicCheck = document.getElementById('strategic-check');
+        this.violenceCheck = document.getElementById('violence-check');
     }
     toggleBusy() {
         this.busyIcon.classList.toggle('invisible');
@@ -38,6 +45,38 @@ class ButtonHandlers {
 
         this.notesBtn.onclick = () => {
             this.notesModal.show();
+        };
+
+        // set switch values to prevent value lost on page reload
+        this.covidSwitch.checked ? this.covidSwitch.value = true : this.covidSwitch.value = false;
+        this.battlesCheck.checked ? this.battlesCheck.value = true : this.battlesCheck.value = false;
+        this.explosionsCheck.checked ? this.explosionsCheck.value = true : this.explosionsCheck.value = false;
+        this.protestsCheck.checked ? this.protestsCheck.value = true : this.protestsCheck.value = false;
+        this.riotsCheck.checked ? this.riotsCheck.value = true : this.riotsCheck.value = false;
+        this.strategicCheck.checked ? this.strategicCheck.value = true : this.strategicCheck.value = false;
+        this.violenceCheck.checked ? this.violenceCheck.value = true : this.violenceCheck.value = false;
+
+        // set switch handlers
+        this.covidSwitch.onclick = () => {
+            this.covidSwitch.value = !this.covidSwitch.value;
+        };
+        this.battlesCheck.onclick = () => {
+            this.battlesCheck.value = !this.battlesCheck.value;
+        };
+        this.explosionsCheck.onclick = () => {
+            this.explosionsCheck.value = !this.explosionsCheck.value;
+        };
+        this.protestsCheck.onclick = () => {
+            this.protestsCheck.value = !this.protestsCheck.value;
+        };
+        this.riotsCheck.onclick = () => {
+            this.riotsCheck.value = !this.riotsCheck.value;
+        };
+        this.strategicCheck.onclick = () => {
+            this.strategicCheck.value = !this.strategicCheck.value;
+        };
+        this.violenceCheck.onclick = () => {
+            this.violenceCheck.value = !this.violenceCheck.value;
         };
     }
 }
