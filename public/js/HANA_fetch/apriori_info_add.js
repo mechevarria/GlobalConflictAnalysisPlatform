200810ac97375c6e20fd1e7ddee3ae81d27445d6
Mike Echevarria <mike.echevarria@gmail.com>
2020-08-18 14:04:34 -0400

    re-add the table parsing function

--- public/js/HANA_fetch/apriori_info_add.js
+++ public/js/HANA_fetch/apriori_info_add.js
@@ -21,141 +21,78 @@ function getParentId(dataElement) {
     }
 }
 
-//FOR CLEANING TEXT OF SECOND AND THIRD LAYERS
-function antecedentClean(dataElement, parentID, secondaryID, tertiaryID) {
+function jsonToTable(data) {
 
-    console.log('INSIDE ANTECEDENTCLEAN');
-    console.log(dataElement);
-
-    var dollarTextPattern = /\$([a-z]|[0-9]| \( | \) | \s| -) {0,40}\$/;
-    var ampPattern = /\$[^*]{0,35}&/;
-
-    if (/&/.test(dataElement.ANTECEDENT)) {
-
-        console.log('IF');
-
-        var extractedDirty = /\$(.*?) &/.exec(dataElement.ANTECEDENT);
-        console.log(extractedDirty[0]);
-
-        var secondLevelDirty = /\$(.*?)\$/.exec(extractedDirty[0]);
-        console.log(secondLevelDirty);
-        var secondLevelClean = secondLevelDirty[0].replace(/\$/ig, '');
-
-        var thirdLevelDirty = /&\$(.*?)\$/.exec(dataElement.ANTECEDENT);
-        console.log(thirdLevelDirty);
-        var thirdLevelClean = thirdLevelDirty[0].replace(/(\$|&)/ig, '');
-
-
-
-
-
-        return [{ id: ((secondaryID + 0.001).toFixed(3)).toString(), parent: parentID.toString(), name: secondLevelClean },
-        { id: ((tertiaryID + 0.001).toFixed(3)).toString(), parent: ((secondaryID + 0.001).toFixed(3)).toString(), name: thirdLevelClean, value: dataElement.SCORE }];
-
-
-    } else {
-
-        console.log('ELSE');
-
-        secondLevelDirty = dataElement.ANTECEDENT;//dollarTextPattern.exec(dataElement.ANTECEDENT);
-
-        //console.log(secondLevelDirty);
-        secondLevelClean = secondLevelDirty.replace(/\$/ig, '');
-        return [{ id: ((secondaryID + 0.001).toFixed(3)).toString(), parent: parentID.toString(), name: secondLevelClean },
-        { id: ((tertiaryID + 0.001).toFixed(3)).toString(), parent: ((secondaryID + 0.001).toFixed(3)).toString(), name: secondLevelClean, value: dataElement.SCORE }];
-
-    }
-}
-
-const vizArray = [{
-    id: '0.0',
-    parent: '',
-    name: 'Event Associations'
-}, {
-    id: '1.1',
-    parent: '0.0',
-    name: 'Protests'
-}, {
-    id: '1.2',
-    parent: '0.0',
-    name: 'Violence Against Civilians'
-}, {
-    id: '1.3',
-    parent: '0.0',
-    name: 'Battles'
-}, {
-    id: '1.4',
-    parent: '0.0',
-    name: 'Explosions'
-}, {
-    id: '1.5',
-    parent: '0.0',
-    name: 'Riots'
-}, {
-    id: '1.6',
-    parent: '0.0',
-    name: 'Strategic Developments'
-}];
-
-function jsonRegextoViz(data) {
-
-    // console.log('JSONTOREGEX');
-    // console.log(data);
-
-    const uniqueEvents = [...new Set(data.map(item => item.CONSEQUENT))]; //getting unique event types
-    console.log(uniqueEvents);
-
-    var secondaryID = 2.000;
-    var tertiaryID = 3.000;
-
-    for (var i = 0; i < uniqueEvents.length; i++) {
-
-        var parentID = getParentId(uniqueEvents[i]);
-
-        // console.log(parentID);
-        //console.log(data.CONSEQUENT);
-
-        var filteredEvent = data.filter((data) => {
-
-            let lft = data.CONSEQUENT.trim();
-            let rgt = uniqueEvents[i].trim();
-            return lft == rgt;
-        });
-
-        console.log(filteredEvent);
-
-        filteredEvent.forEach(element => {
-
-            var cleanResults = antecedentClean(element, parentID, secondaryID, tertiaryID);
-            console.log(cleanResults);
-
-            secondaryID = Number(parseFloat(cleanResults[0].id).toFixed(3));
-            tertiaryID = Number(parseFloat(cleanResults[1].id).toFixed(3));
-
-            console.log(secondaryID);
-            console.log(tertiaryID);
-
-            //(cleanResults.length > 1) ? tertiaryID = parseInt(cleanResults[1].id) : console.log('No tertiary level detected');
-
-            cleanResults.forEach((obj) => {
-                vizArray.push(obj);
-            });
-
-            // console.log(vizArray);
-
-        });
-
+    // EXTRACT VALUE FOR HTML HEADER. 
+    var col = [];
+    for (var i = 0; i < data.length; i++) {
+        for (var key in data[i]) {
+            if (col.indexOf(key) === -1) {
+                col.push(key);
+            }
+        }
     }
 
-    sunChart.series[0].setData(vizArray);
+    // CREATE DYNAMIC TABLE.
+    var table = document.getElementById('apriori-info');
+    // clear old data first
+    table.innerHTML = '';
+
+    // CREATE HTML TABLE HEADER ROW USING THE EXTRACTED HEADERS ABOVE.
+    var tr = table.insertRow(-1);                   // TABLE ROW.
+
+    for (i = 0; i < col.length; i++) {
+        var th = document.createElement('th');      // TABLE HEADER.
+        th.innerHTML = col[i];
+        tr.appendChild(th);
+    }
+
+    // ADD JSON DATA TO THE TABLE AS ROWS.
+    for (i = 0; i < data.length; i++) {
+
+        tr = table.insertRow(-1);
+
+        for (var j = 0; j < col.length; j++) {
+            var tabCell = tr.insertCell(-1);
+            tabCell.innerHTML = data[i][col[j]];
+        }
+    }
 }
 
 function dataToViz(secondLevel) {
-    let chartData = vizArray;
+    let chartData = [{
+        id: '0.0',
+        parent: '',
+        name: 'Event Associations'
+    }, {
+        id: '1.1',
+        parent: '0.0',
+        name: 'Protests'
+    }, {
+        id: '1.2',
+        parent: '0.0',
+        name: 'Violence Against Civilians'
+    }, {
+        id: '1.3',
+        parent: '0.0',
+        name: 'Battles'
+    }, {
+        id: '1.4',
+        parent: '0.0',
+        name: 'Explosions'
+    }, {
+        id: '1.5',
+        parent: '0.0',
+        name: 'Riots'
+    }, {
+        id: '1.6',
+        parent: '0.0',
+        name: 'Strategic Developments'
+    }];
 
     const thirdLevel = [];
 
-    secondLevel.forEach((obj,index) => {
+    secondLevel.forEach((obj, index) => {
         const parentId = getParentId(obj.CONSEQUENT);
         obj.parent = parentId;
         obj.id = `2.${index}`;
@@ -170,7 +107,7 @@ function dataToViz(secondLevel) {
                 value: obj.SCORE
             };
             thirdLevel.push(newObj);
-            
+
         } else {
             obj.name = obj.ANTECEDENT;
             const newObj = {
@@ -199,7 +136,7 @@ function dataToViz(secondLevel) {
 
     // fix parent ids of third level were merged
     thirdLevel.forEach(obj => {
-        if(mergedIds.has(obj.parent)) {
+        if (mergedIds.has(obj.parent)) {
             obj.parent = mergedIds.get(obj.parent);
         }
     });
@@ -208,10 +145,10 @@ function dataToViz(secondLevel) {
 
     // remove dollar signs from name
     chartData = chartData.map(obj => {
-        
-        obj.name = obj.name.replace(/\$/g,'');
+
+        obj.name = obj.name.replace(/\$/g, '');
         return obj;
-       
+
     });
 
     sunChart.series[0].setData(chartData);
@@ -248,8 +185,8 @@ function apriori_info_get(country_capital) {
                 throw new Error(res.error);
             }
 
-            //jsonRegextoViz(res.data);
             dataToViz(res.data);
+            jsonToTable(res.data);
             btnHandlers.eventBtn.disabled = false;
             lda_info_get(country_capital);
 
