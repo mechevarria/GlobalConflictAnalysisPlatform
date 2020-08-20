4d517c31df21b2f88bf139a1a4cc9f2219729bc7
aajulianNS2 <aaron.julian@sapns2.com>
2020-08-20 16:32:47 -0400

    Getting my build back up to date

--- cf-push-buildpack.sh
+++ cf-push-buildpack.sh
@@ -1,21 +1,3 @@
-<<<<<<< HEAD
-#!/usr/bin/env bash
-
-app=gca-express-buildpack
-
-cf push $app \
-    -b nodejs_buildpack \
-    --no-start \
-    -m 256M \
-    -k 1048M
-
-cf se $app HDB_HOST $HDB_HOST
-cf se $app HDB_PORT $HDB_PORT
-cf se $app HDB_USER $HDB_USER
-cf se $app HDB_PASSWORD $HDB_PASSWORD
-
-cf start $app
-=======
 #!/usr/bin/env bash
 
 service=gca-db
@@ -36,4 +18,3 @@ cf push $app \
 cf bind-service $app $service
 
 cf start $app
->>>>>>> e3bc7f9c531557d215ebf77219248ced22a9102e
--- cf-push-docker.sh
+++ cf-push-docker.sh
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-#!/usr/bin/env bash
-
-app=gca-express
-
-cf push $app \
-    -m 128M \
-    -k 2048M \
-    --docker-image quay.io/mechevarria/$app \
-    --no-start
-
-cf se $app HDB_HOST $HDB_HOST
-cf se $app HDB_PORT $HDB_PORT
-cf se $app HDB_USER $HDB_USER
-cf se $app HDB_PASSWORD $HDB_PASSWORD
-
-=======
 #!/usr/bin/env bash
 
 service=gca-db
@@ -34,5 +17,4 @@ cf push $app \
 # bind hana service
 cf bind-service $app $service
 
->>>>>>> e3bc7f9c531557d215ebf77219248ced22a9102e
 cf start $app
